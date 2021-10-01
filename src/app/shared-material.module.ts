7af57d41bbb3ca9c14e65a9da588da2fb3699e81
Nicolas Riot <nriot@umoove-inc.com>
2021-10-01 18:01:17 +0200

    content

--- src/app/shared-material.module.ts
+++ src/app/shared-material.module.ts
@@ -5,11 +5,22 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
+  imports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
+  ],
   exports: [
     MatToolbarModule,
+    MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
