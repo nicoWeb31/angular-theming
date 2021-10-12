bfdc603a2bbf2e72d1a85c09efd6703a2bb41aaf
Nicolas Riot <nriot@umoove-inc.com>
2021-10-12 19:18:46 +0200

    compoment custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedMaterialModule } from './shared-material.module';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
