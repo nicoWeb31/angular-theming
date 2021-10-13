48fca2ddd12ad5903311581d891a334562b9b174
Nicolas Riot <nriot@umoove-inc.com>
2021-10-13 20:30:22 +0200

    split theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-theming';
-  text= 'ma banniere '
+  text = 'ma banniere ';
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
+  chooseTheme(e: string) {
+    if (e === 'dark') {
+      this.currentTheme = 'dark-theme';
+    }
+    if (e === 'light') {
+      this.currentTheme = 'light-theme';
+    }
+    console.log(this.currentTheme);
+  }
 }
