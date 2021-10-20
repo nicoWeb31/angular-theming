3ac7563d855e2326f3539e167e31995b2f7c9301
Nicolas Riot <nriot@umoove-inc.com>
2021-10-20 10:13:04 +0200

    dark light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
-import { Component, HostBinding } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  HostBinding,
+  Inject,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-theming';
   text = 'ma banniere ';
-  @HostBinding('class')
-  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+  // @HostBinding('class')
+  // currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private rendered: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.rendered.removeClass(this.document.body, 'light-theme');
+    this.rendered.addClass(this.document.body, 'dark-theme');
+  }
 
   chooseTheme(e: string) {
     if (e === 'dark') {
-      this.currentTheme = 'dark-theme';
+      // this.currentTheme = 'dark-theme';
+      this.rendered.removeClass(this.document.body, 'light-theme');
+      this.rendered.addClass(this.document.body, 'dark-theme');
     }
     if (e === 'light') {
-      this.currentTheme = 'light-theme';
+      // this.currentTheme = 'light-theme';
+      this.rendered.removeClass(this.document.body, 'dark-theme');
+      this.rendered.addClass(this.document.body, 'light-theme');
     }
-    console.log(this.currentTheme);
+    // console.log(this.currentTheme);
   }
 }
